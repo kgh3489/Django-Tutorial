3a3cfadad07bbed93311ffb5cda4d18bdbb77fb2
kgh <kgh3489@naver.com>
2022-01-17 22:37:55 +0900

    change to generic view

--- polls/urls.py
+++ polls/urls.py
@@ -4,13 +4,10 @@ from . import views
 
 app_name = 'polls'
 urlpatterns = [
-    #클라이언트가 polls를 호출하면 views.index라는 view를 호출
-    #views.py에서 index의 request인 Hello world를 반환
-    path('', views.index, name='index'),
-    #path마다 동일 과정
-    path('<int:question_id>/', views.detail, name='detail'),
-    path('<int:question_id>/results/', views.results, name='results'),
+    path('', views.IndexView.as_view(), name='index'),
+    path('<int:pk>/', views.DetailView.as_view(), name='detail'),
+    path('<int:pk>/results/', views.ResultsView.as_view(), name='results'),
     path('<int:question_id>/vote/', views.vote, name='vote'),
-] 
+]
 #<int:question_id>/vote/ 는 장고 url패턴. 이 패턴대로 입력되면 그에 해당되는 view를 호출
 #views.py의 파라미터(question_id)와 동일해야됨!
--- polls/views.py
+++ polls/views.py
@@ -4,15 +4,17 @@ from django.http.response import HttpResponse
 from django.http import HttpResponse, HttpResponseRedirect
 from django.template import loader
 from django.urls import reverse
+from django.views import generic
 from django.shortcuts import render, get_object_or_404 #단축기능 제공
+from .models import Choice, Question
 
+'''
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    template = loader.get_template('polls/index.html')
-    context = {'latest_question_list': latest_question_list}
+    #template = loader.get_template('polls/index.html')
+    context = {'latest_question_list': latest_question_list} # temlplate에 데이터를 전달
     #return HttpResponse(template.render(context, request))
     return render(request, 'polls/index.html', context) # render를 사용하여 HttpResponse와 request를 사용 안 함.
-    #context = {'latest_question_list': latest_question_list,} # temlplate에 데이터를 전달
     #output = ', '.join([q.question_text for q in latest_question_list]) # Question 데이터 중에서 출판일자를 정렬하여 5개까지만 가져오고 그것을 콤마로 연결
 
 # 새로운 index()뷰 하나를 호출했을 때,
@@ -32,12 +34,13 @@ def detail(request, question_id):
     #3
     question = get_object_or_404(Question, pk=question_id)
     return render(request, 'polls/detail.html', {'question': question})
+
 def results(request, question_id):
-    return HttpResponse(response % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/results.html', {'question': question})
 
 def vote(request, question_id):
-    #return HttpResponse("You're voting on question %s." % question_id)
-     question = get_object_or_404(Question, pk=question_id)
+    question = get_object_or_404(Question, pk=question_id)
     try:
         selected_choice = question.choice_set.get(pk=request.POST['choice'])
     except (KeyError, Choice.DoesNotExist):
@@ -55,3 +58,39 @@ def vote(request, question_id):
         return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
 
 #클라이언트로부터 request를 받으면 response를 돌려줌
+'''
+
+#generic view
+
+class IndexView(generic.ListView):
+    template_name = 'polls/index.html'
+    context_object_name = 'latest_question_list'
+
+    def get_queryset(self):
+        """Return the last five published questions."""
+        return Question.objects.order_by('-pub_date')[:5]
+
+
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = 'polls/detail.html'
+
+
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = 'polls/results.html'
+
+
+def vote(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        return render(request, 'polls/detail.html', {
+            'question': question,
+            'error_message': "You didn't select a choice.",
+        })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
